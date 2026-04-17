3aacf2f17a7ca7df69af91d629269bb8a37a4378
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-04-17 08:49:01 +0200

    Add tests for validation of integration variable names.

---
+++ api-orchestrator/internal/api/validate_test.go
@@ -0,0 +1,57 @@
+package api
+
+import "testing"
+
+func validCreateJobRequest() CreateJobRequest {
+	return CreateJobRequest{
+		Name:      "test-job",
+		Integrand: "x + 1",
+		IntegrationVariables: []VariableSpec{
+			{
+				Name:  "x",
+				Lower: "0",
+				Upper: "1",
+			},
+		},
+		Evaluations: 1000,
+	}
+}
+
+func TestValidateBasicAcceptsValidVariableNames(t *testing.T) {
+	validNames := []string{
+		"x",
+		"x1",
+		"x_1",
+		"Alpha",
+		"A2_b",
+	}
+
+	for _, name := range validNames {
+		req := validCreateJobRequest()
+		req.IntegrationVariables[0].Name = name
+
+		if err := req.ValidateBasic(); err != nil {
+			t.Fatalf("expected variable name %q to be valid, got error: %v", name, err)
+		}
+	}
+}
+
+func TestValidateBasicRejectsInvalidVariableNames(t *testing.T) {
+	invalidNames := []string{
+		"",
+		"_x",
+		"1x",
+		"x-y",
+		"x y",
+	}
+
+	for _, name := range invalidNames {
+		req := validCreateJobRequest()
+		req.IntegrationVariables[0].Name = name
+
+		err := req.ValidateBasic()
+		if err == nil {
+			t.Fatalf("expected variable name %q to be invalid, got nil error", name)
+		}
+	}
+}
