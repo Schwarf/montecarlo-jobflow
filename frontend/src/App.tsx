176ec210b003c40ad71a2b23aace9f779985a5a0
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-04-17 11:48:28 +0200

    Add App component rendering job submission form

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { JobForm } from "./components/JobForm";
+
+function App() {
+    return (
+        <div>
+            <h1>Monte Carlo Job Submission</h1>
+            <JobForm />
+        </div>
+    );
+}
+
+export default App;
