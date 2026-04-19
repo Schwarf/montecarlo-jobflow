962deb937a6d27f99a8f300f0707514feda1ca21
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-04-19 15:53:04 +0200

    Implement GetByID API.

--- api-orchestrator/internal/job/sqlite/respository.go
+++ api-orchestrator/internal/job/sqlite/respository.go
@@ -62,7 +62,78 @@ INSERT INTO jobs (
 }
 
 func (r *Repository) GetByID(ctx context.Context, id string) (job.Job, error) {
-	panic("not implemented")
+	const query = `
+SELECT
+	job_id,
+    name,
+	integrand,
+	variables_json,
+	evaluations,
+	status,
+	error_message,
+	result_json,
+	created_at,
+	updated_at
+	FROM jobs
+	WHERE job_id = ?;`
+
+	var (
+		j            job.Job
+		variableJSON string
+		status       string
+		errorMessage sql.NullString
+		resultJSON   sql.NullString
+		createdAt    string
+		updatedAt    string
+	)
+
+	err := r.db.QueryRowContext(ctx, query, id, id).Scan(
+		&j.ID,
+		&j.Name,
+		&j.Integrand,
+		&variableJSON,
+		j.Evaluations,
+		&status,
+		&errorMessage,
+		&resultJSON,
+		&createdAt,
+		&updatedAt,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return job.Job{}, fmt.Errorf("job %q not found: %w", id, err)
+		}
+		return job.Job{}, fmt.Errorf("query job by id: %w", err)
+	}
+
+	if err := json.Unmarshal([]byte(variableJSON), &j.IntegrationVariables); err != nil {
+		return job.Job{}, fmt.Errorf("unmarshal integration variables: %w", err)
+	}
+
+	j.Status = job.Status(status)
+
+	if errorMessage.Valid {
+		j.ErrorMessage = &errorMessage.String
+	}
+
+	if resultJSON.Valid {
+		j.ResultJSON = &resultJSON.String
+	}
+
+	parsedCreatedAt, err := time.Parse(time.RFC3339, createdAt)
+
+	if err != nil {
+		return job.Job{}, fmt.Errorf("parse created at: %w", err)
+	}
+	j.CreatedAt = parsedCreatedAt
+
+	parsedUpdatedAt, err := time.Parse(time.RFC3339, updatedAt)
+	if err != nil {
+		return job.Job{}, fmt.Errorf("parse updated at: %w", err)
+	}
+	j.UpdatedAt = parsedUpdatedAt
+
+	return j, nil
 }
 
 var _ job.Repository = (*Repository)(nil)
