871e8695bc3b8a292a384ba83b79aed63144b645
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-04-26 16:24:51 +0200

    Remove code-gen first steps and corresponding tests.

--- api-orchestrator/internal/expr/cpp_codegen.go
+++ api-orchestrator/internal/expr/cpp_codegen.go
@@ -7,35 +7,6 @@ import (
 
 type CppCodeGenerator struct{}
 
-func (g *CppCodeGenerator) GenerateFunction(functionName string, variableNames []string, returnExpression string) (string, error) {
-	if functionName == "" {
-		return "", fmt.Errorf("function name must not be empty")
-	}
-
-	var builder strings.Builder
-
-	builder.WriteString("double ")
-	builder.WriteString(functionName)
-	builder.WriteString("(")
-
-	for i, variableName := range variableNames {
-		if i > 0 {
-			builder.WriteString(", ")
-		}
-
-		builder.WriteString("double ")
-		builder.WriteString(variableName)
-	}
-
-	builder.WriteString(") {\n")
-	builder.WriteString("    return ")
-	builder.WriteString(returnExpression)
-	builder.WriteString(";\n")
-	builder.WriteString("}\n")
-
-	return builder.String(), nil
-}
-
 func (g *CppCodeGenerator) GenerateExpression(expr Expression) (string, error) {
 	switch e := expr.(type) {
 	case *NumberExpression:
--- api-orchestrator/internal/expr/cpp_codegen_test.go
+++ api-orchestrator/internal/expr/cpp_codegen_test.go
@@ -2,49 +2,6 @@ package expr
 
 import "testing"
 
-func TestCppCodeGeneratorGenerateFunctionWithoutVariables(t *testing.T) {
-	generator := &CppCodeGenerator{}
-
-	code, err := generator.GenerateFunction("evaluate", nil, "1.0")
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	expected := "double evaluate() {\n" +
-		"    return 1.0;\n" +
-		"}\n"
-
-	if code != expected {
-		t.Fatalf("unexpected generated code:\nexpected:\n%s\nactual:\n%s", expected, code)
-	}
-}
-
-func TestCppCodeGeneratorGenerateFunctionWithVariables(t *testing.T) {
-	generator := &CppCodeGenerator{}
-
-	code, err := generator.GenerateFunction("evaluate", []string{"x", "y", "z"}, "x + y + z")
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-
-	expected := "double evaluate(double x, double y, double z) {\n" +
-		"    return x + y + z;\n" +
-		"}\n"
-
-	if code != expected {
-		t.Fatalf("unexpected generated code:\nexpected:\n%s\nactual:\n%s", expected, code)
-	}
-}
-
-func TestCppCodeGeneratorRejectsEmptyFunctionName(t *testing.T) {
-	generator := &CppCodeGenerator{}
-
-	_, err := generator.GenerateFunction("", []string{"x"}, "x")
-	if err == nil {
-		t.Fatal("expected error, got nil")
-	}
-}
-
 func TestCppCodeGeneratorGenerateNumberExpression(t *testing.T) {
 	generator := &CppCodeGenerator{}
 
